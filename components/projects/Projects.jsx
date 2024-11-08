2d141428b96e22db1f039555086adb0d5829e3f2
Hadil <hadilbenabdallah111@gmail.com>
2024-11-08 21:57:05 +0100

    Update projects section

--- components/projects/Projects.jsx
+++ components/projects/Projects.jsx
@@ -1,12 +1,17 @@
 import HyperOne from '../customH1/HyperOne';
+import Image from 'next/image';
 
 export default function Projects() {
   return (
     <div id="projects" className="flex flex-col my-16">
       <HyperOne value={'Projects'} />
-      <p className="text-center mt-2 lg:mx-32 lg:text-2xl sm:text-2xl">
-        Loading...
-      </p>
+      <div className="flex justify-center items-center mt-2">
+        <img 
+          src="imgs/underconstruction.png"
+          alt="Under Construction"
+          className="w-60 h-60 object-contain"
+        />
+      </div>
     </div>
   );
 }
