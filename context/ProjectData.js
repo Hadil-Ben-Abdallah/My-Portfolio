b3fd497a6e2eeccf2d88784ccf77ab564246bd8f
Hadil <hadilbenabdallah111@gmail.com>
2025-01-23 16:01:29 +0100

    Add first project data :card_file_box:

---
+++ context/ProjectData.js
@@ -0,0 +1,17 @@
+import { FaHtml5, FaCss3Alt, FaJs } from 'react-icons/fa';
+
+export const PROJECTS = [
+  {
+    projectImage: '/imgs/project1.png',
+    headline: 'Communications & Systems',
+    techStack: [
+      { icon: FaHtml5, color: '#E34F26' },
+      { icon: FaCss3Alt, color: '#1572B6' },
+      { icon: FaJs, color: '#F7DF1E' },
+    ],
+    description: `A dynamic web page for the company Communications & Systems, marking the start of my journey with JavaScript development.
+      Designed an engaging, user-friendly interface and implemented interactive features.`,
+    liveTestLink: 'https://communications-and-systems.vercel.app/',
+    gitLink: 'https://github.com/Hadil-Ben-Abdallah/Communications-and-systems',
+  },
+];
