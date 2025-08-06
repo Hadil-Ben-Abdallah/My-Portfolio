ee3a417b3a750b9389f4bf1fb3536fa78e6388af
Hadil <hadilbenabdallah111@gmail.com>
2025-08-06 14:13:41 +0100

    Format the CV button code :art:

--- components/button/CustomButton.jsx
+++ components/button/CustomButton.jsx
@@ -1,10 +1,9 @@
-
 import styles from './button.module.css';
 const CustomButton = ({ text, onclick }) => {
   return (
     <button
       onClick={onclick}
-      class="group relative inline-block overflow-hidden border-2 border-gray-100 bg-main  px-12 py-3 text-xl font-medium text-white hover:text-primary focus:outline-none hover:border-transparent "
+      class="group relative inline-block overflow-hidden border-2 border-black dark:border-gray-100 bg-main px-12 py-3 text-xl font-medium text-black dark:text-white hover:text-primary dark:hover:text-primary focus:outline-none hover:border-transparent dark:hover:border-transparent"
     >
       <span class="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-primary transition-all duration-300 group-hover:w-full"></span>
       <span class="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-primary transition-all duration-500 group-hover:h-full"></span>
